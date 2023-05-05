c8d20563223bab5af293024b2c09575712b4c49f
Kayila <arekuanubis@gmail.com>
2023-05-05 19:56:53 -0400

    Removed more unneeded packages, updated user-data to create default user

--- stage2/02-net-tweaks/01-run.sh
+++ stage2/02-net-tweaks/01-run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
-on_chroot << EOF
-	SUDO_USER="${FIRST_USER_NAME}" raspi-config nonint do_boot_wait 0
-#	SUDO_USER="${FIRST_USER_NAME}" raspi-config nonint do_netconf 1
-	SUDO_USER="${FIRST_USER_NAME}" raspi-config nonint do_net_names 0
-EOF
+# on_chroot << EOF
+# 	SUDO_USER="${FIRST_USER_NAME}" raspi-config nonint do_boot_wait 0
+# #	SUDO_USER="${FIRST_USER_NAME}" raspi-config nonint do_netconf 1
+# 	SUDO_USER="${FIRST_USER_NAME}" raspi-config nonint do_net_names 0
+# EOF
