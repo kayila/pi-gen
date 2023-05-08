d70a439db35e368aab58f408be5f16d30b1b1fe3
Kayila <arekuanubis@gmail.com>
2023-05-07 21:29:53 -0400

    Switched to using systemd-networkd for network stuff

--- export-image/03-network/01-run.sh
+++ export-image/03-network/01-run.sh
@@ -1,3 +1,11 @@
 #!/bin/bash -e
 
-install -m 644 files/resolv.conf "${ROOTFS_DIR}/etc/"
+install -m 644 files/04-eth0.network     "${ROOTFS_DIR}/etc/systemd/network/04-eth0.network"
+
+on_chroot << EOF
+
+ln -rsf /run/systemd/resolve/stub-resolv.conf /etc/resolv.conf
+systemctl disable networking
+systemctl enable systemd-networkd systemd-resolved
+
+EOF
