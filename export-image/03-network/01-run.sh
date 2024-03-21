76c779713998bf4f7c9dd3c5446164e9975351ef
Kayila <arekuanubis@gmail.com>
2024-03-21 04:00:59 -0400

    Changes for predictable interface names

--- export-image/03-network/01-run.sh
+++ export-image/03-network/01-run.sh
@@ -1,6 +1,10 @@
 #!/bin/bash -e
 
-install -m 644 files/04-main.network     "${ROOTFS_DIR}/etc/systemd/network/04-main.network"
+install -m 644 files/50-main.network     "${ROOTFS_DIR}/etc/systemd/network/50-main.network"
+
+install -m 644 files/01-net.link         "${ROOTFS_DIR}/etc/systemd/network/01-net.link"
+install -m 644 files/02-usb.link         "${ROOTFS_DIR}/etc/systemd/network/02-usb.link"
+install -m 644 files/03-wifi.link        "${ROOTFS_DIR}/etc/systemd/network/03-wifi.link"
 
 on_chroot << EOF
 
