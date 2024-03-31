9331ab6dafb3984aea58a4e18ac1048b3004d6fa
Kayila <arekuanubis@gmail.com>
2024-03-30 20:45:07 -0400

    Added Buster backports for newer systemd.

--- export-image/03-network/01-run.sh
+++ export-image/03-network/01-run.sh
@@ -1,5 +1,17 @@
 #!/bin/bash -e
 
+
+if [ "${RELEASE}" = "buster" ]; then
+	echo "Debian buster, need to pull in a backported systemd"
+
+	install -m 644 files/buster-backports.list "${ROOTFS_DIR}/etc/apt/sources.list.d/buster-backports.list"
+	on_chroot <<- EOF
+	apt-get update
+
+	apt-get install -y -t buster-backports systemd udev
+	EOF
+fi
+
 install -m 644 files/50-main.network     "${ROOTFS_DIR}/etc/systemd/network/50-main.network"
 
 install -m 644 files/01-net.link         "${ROOTFS_DIR}/etc/systemd/network/01-net.link"
